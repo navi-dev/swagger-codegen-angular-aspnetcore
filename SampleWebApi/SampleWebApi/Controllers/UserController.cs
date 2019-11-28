3d7853ee2b6cda48b43da759a04a6f5f4db46837
Naveen Motwani <v435576@amerisourcebergen.com>
2019-11-28 10:12:10 +0530

    Added user related functionality

---
+++ SampleWebApi/SampleWebApi/Controllers/UserController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SampleWebApi.Model;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SampleWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        public static List<User> users = new List<User>()
+        {
+                new User(1, "Naveen", "Motwani", "naveen@gmail.com", "developer"),
+                new User(2, "Gaurav", "Patel", "Gaurav@gmail.com", "manager"),
+                new User(3, "Pavan", "Bandi", "Pavan@gmail.com", "lead"),
+                 new User(4, "Lav", "Gupta", "naveen@gmail.com", "Architect")
+        };
+
+        [HttpGet("getAllUsers")]
+        public List<User> GetUsers()
+        {
+            return users;
+        }
+
+        [HttpGet]
+        [Route("getUser/{userId}")]
+        public User GetUser(int userId)
+        {
+            var user = users.FirstOrDefault(elem => elem.Id == userId);
+            return user;
+        }
+
+        [HttpPost]
+        [Route("addUser")]
+        public User AddUser(User user)
+        {
+            users = users.OrderBy(elem => elem.Id).ToList();
+            user.Id = users.Last().Id + 1;
+            users.Add(user);
+
+            return user;
+        }
+
+        [HttpPost]
+        [Route("deleteUser")]
+        public string DeleteUser(int userId)
+        {
+            string message = "User not found";
+            var user = users.FirstOrDefault(elem => elem.Id == userId);
+            if (user != null)
+            {
+                users.Remove(user);
+
+                message = "Deleted";
+            }
+
+            return message;
+        }
+    }
+}
---
+++ SampleWebApi/SampleWebApi/Model/User.cs
@@ -0,0 +1,25 @@
+﻿namespace SampleWebApi.Model
+{
+    public class User
+    {
+        public User()
+        {
+
+        }
+
+        public User(int id, string firstName, string lastName, string emailId, string designation)
+        {
+            Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+            EmailId = emailId;
+            Designation = designation;
+        }
+
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailId { get; set; }
+        public string Designation { get; set; }
+    }
+}
